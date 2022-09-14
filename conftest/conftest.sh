d02fe3200ae0d3b094030c4fd274da323b4bf928
tobias-jenkner <tobias.jenkner@googlemail.com>
2022-09-14 16:29:32 +0200

    Conftest (#3)
    * debug output
    
    * fix change check for flux kustomization file
    
    * fix change check for flux kustomization file
    
    * fix change check for flux kustomization file
    
    * fix change check for flux kustomization file
    
    * debug output
    
    * debug output
    
    * debug output
    
    * debug output
    
    * change file test
    
    * change file test
    
    * change file test
    
    * multi doc support
    
    * multi doc support
    
    * multi doc support
    
    * contest & kustomization yaml generation actions
    
    * updates
    
    * fix existing kustomization.yaml check
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * no multiline outputs in gh action variables
    
    Co-authored-by: Tobias Jenkner <tobias.jenkner@differenzierbar.de>

---
+++ conftest/conftest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# find all policy directories in parent folders 
+git_root_dir=$(git rev-parse --show-toplevel)
+
+set -e
+path="$(readlink -f "$1")"
+root_dir="$(readlink -f "$2")"
+policy_folders=()
+
+while : ; do
+    # echo $path
+    policy_folders+=($(find "$path" -maxdepth 1 -mindepth 1 -name "policy" -type d))
+    [[ $path != $root_dir ]] && [[ $path != "/" ]] || break
+    path="$(readlink -f "$path"/..)"
+done
+
+# execute conftest
+if [[ ${#policy_folders[@]} > 0 ]];then
+    echo "executing 'conftest test ${policy_folders[@]/#/"-p "} - <&0'"
+    conftest test ${policy_folders[@]/#/"-p "} - <&0
+else
+    echo "no policy folders found - skipping contest"
+fi
---
+++ generate-kustomization-yaml/generate-kustomization-yaml.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+path="$(readlink -f "$1")"
+if [[ ! -f "$path/kustomization.yaml" ]]; then
+    pushd $path
+    # FIMXE recursive find yaml files
+    kustomize create --autodetect --recursive
+    cat kustomization.yaml
+    popd
+fi
