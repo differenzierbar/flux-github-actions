c3d5e408ee399dc64c712af4e06866a1273b6101
Tobias Jenkner <tobias.jenkner@differenzierbar.de>
2022-09-15 21:52:46 +0200

    conftest with multiple files

--- conftest/conftest-test/conftest.sh
+++ conftest/conftest-test/conftest.sh
@@ -4,22 +4,25 @@
 git_root_dir=$(git rev-parse --show-toplevel)
 
 set -e
-file="$(readlink -f "$1")"
-path="$(dirname $file)"
-root_dir="$(readlink -f "$2")"
-policy_folders=()
 
+policy_parent_directory_top="$(readlink -f "$1")"
+shift
+policy_parent_directory_bottom="$(readlink -f "$1")"
+shift
+
+policy_folders=()
+path=$policy_parent_directory_bottom
 while : ; do
     # echo $path
     policy_folders+=($(find "$path" -maxdepth 1 -mindepth 1 -name "policy" -type d))
-    [[ $path != $root_dir ]] && [[ $path != "/" ]] || break
+    [[ $path != $policy_parent_directory_top ]] && [[ $path != "/" ]] || break
     path="$(readlink -f "$path"/..)"
 done
 
 # execute conftest
 if [[ ${#policy_folders[@]} > 0 ]];then
     echo "executing 'conftest test $file ${policy_folders[@]/#/"-p "} -o github'"
-    conftest test $file ${policy_folders[@]/#/"-p "} -o github
+    conftest test $@ ${policy_folders[@]/#/"-p "} -o github
 else
     echo "no policy folders found - skipping contest"
 fi
