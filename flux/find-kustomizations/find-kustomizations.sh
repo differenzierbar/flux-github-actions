be8166256b9b02f71256510c9be1d27bc788a993
Tobias Jenkner <tobias.jenkner@differenzierbar.de>
2022-09-16 20:42:27 +0200

    introduce get-kustomization-path part 2

--- flux/find-kustomizations/find-kustomizations.sh
+++ flux/find-kustomizations/find-kustomizations.sh
@@ -4,15 +4,5 @@ set -e
 kustomizations_root=$1
 flux_kustomization_files=$(find $kustomizations_root -name "*.yml" -exec yq -N eval-all '. | select(.kind == "Kustomization" and .apiVersion == "kustomize.toolkit.fluxcd.io/v1beta2") | filename' {} +)
 
-result=()
-while IFS= read -r kustomization_file; do
-    result+=($(yq -N eval-all '. | select(.kind == "Kustomization" and .apiVersion == "kustomize.toolkit.fluxcd.io/v1beta2") | .spec.path' "$kustomization_file"))
-done <<< "${flux_kustomization_files}"
-
-IFS=" " read -r -a result <<< "$(echo "${result[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')"
-echo ${result[@]}
-
-
-
-
-
+IFS=" " read -r -a flux_kustomization_files <<< "$(echo "${flux_kustomization_files[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')"
+echo ${flux_kustomization_files[@]}
--- flux/get-kustomization-path/get-kustomization-path.sh
+++ flux/get-kustomization-path/get-kustomization-path.sh
@@ -4,9 +4,3 @@ set -e
 kustomization_file=$1
 
 yq -N eval-all '. | select(.kind == "Kustomization" and .apiVersion == "kustomize.toolkit.fluxcd.io/v1beta2") | .spec.path' "$kustomization_file"
-
-
-
-
-
-
