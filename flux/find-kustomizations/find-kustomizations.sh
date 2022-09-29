307d7c54e55cf940ed30b034a380fb9af058913a
Tobias Jenkner <tobias.jenkner@differenzierbar.de>
2022-09-29 10:32:08 +0200

    filter kustomizations based on source name

--- flux/find-kustomizations/find-kustomizations.sh
+++ flux/find-kustomizations/find-kustomizations.sh
@@ -2,7 +2,10 @@
 set -e
 
 kustomizations_root=$1
-flux_kustomization_files=$(find $kustomizations_root -name "*.yml" -exec yq -N eval-all '. | select(.kind == "Kustomization" and .apiVersion == "kustomize.toolkit.fluxcd.io/v1beta2") | filename' {} +)
+git_repository=$2
+
+flux_kustomization_files=$(find $kustomizations_root -name "*.yml" -exec yq -N eval-all ". | select(.kind == \"Kustomization\" and .apiVersion == \"kustomize.toolkit.fluxcd.io/v1beta2\" and .spec.sourceRef.name == \"$git_repository\") | filename" {} +)
+
 
 IFS=" " read -r -a flux_kustomization_files <<< "$(echo "${flux_kustomization_files[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')"
 echo ${flux_kustomization_files[@]}
