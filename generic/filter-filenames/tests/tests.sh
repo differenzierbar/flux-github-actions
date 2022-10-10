8206b0e797b87e132b954b7007dadb86fa988051
Tobias Jenkner <tobias.jenkner@differenzierbar.de>
2022-10-10 14:31:48 +0200

    use assert function

--- generic/filter-filenames/tests/tests.sh
+++ generic/filter-filenames/tests/tests.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 
 here=`dirname $(realpath $0)`
+. $here/../../../.tests/assert
 
-separator=' '
+export SEPARATOR=' '
 
 result=$($here/../filter-filenames.sh '\.ya?ml$' "matching/invalid.yml notmatching/valid matching.yaml matching/valid.yml notmatching/valid.txt notmatching.xml" "$separator")
-echo $result
-result_array=()
-IFS="$separator" read -r -a result_array <<< "$result"
-
-if [[ ${#result_array[@]} != 3 ]]; then
-    echo "3 elements expected"
-    exit 1
-fi
-
-if [[ "${result_array[0]}" != "matching/invalid.yml" ]]; then
-    echo "matching/invalid.yml expected"
-    exit 1
-fi
-
-if [[ "${result_array[1]}" != "matching/valid.yml" ]]; then
-    echo "matching/valid.yml expected"
-    exit 1
-fi
-
-if [[ "${result_array[2]}" != "matching.yaml" ]]; then
-    echo "matching.yaml expected"
-    exit 1
-fi
+assert "matching/invalid.yml matching/valid.yml matching.yaml" "$result"
