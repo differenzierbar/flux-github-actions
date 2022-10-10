b123f73b6df34a0dede62039b05ad12ec63ceaa7
Tobias Jenkner <tobias.jenkner@differenzierbar.de>
2022-10-10 12:23:27 +0200

    fix test

--- flux/find-kustomizations/tests/tests.sh
+++ flux/find-kustomizations/tests/tests.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
 here=`dirname $(realpath $0 --relative-to .)`
+. $here/../../../.tests/assert
 
 export SEPARATOR=' '
 
 query='.spec.sourceRef.name=="flux-system"'
 
 result="$($here/../find-kustomizations.sh $here/testdata "$query")"
-result_array=()
-
-IFS="$separator" read -r -a result_array <<< "$result"
-echo "result: ${result_array[@]}"
-
-if [[ ${#result_array[@]} != 2 ]]; then
-    echo "2 elements expected"
-    exit 1
-fi
-
-if [[ "${result_array[0]}" != "$here/testdata/testdata.yml?metadata.name==\"one\"" ]]; then
-    echo "$here/testdata/testdata.yml?metadata.name==one expected"
-    exit 1
-fi
-
-if [[ "${result_array[1]}" != "$here/testdata/testdata.yml?metadata.name==\"two\"" ]]; then
-    echo "$here/testdata/testdata.yml?metadata.name==two expected"
-    exit 1
-fi
+assert "$here/testdata/testdata.yml?metadata.name==\"one\" $here/testdata/testdata.yml?metadata.name==\"two\"" "$result"
