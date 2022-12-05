863b1e1f9b1f656fea3cf57db1a7143af33b39bc
Tobias Jenkner <tobias.jenkner@gmail.com>
2022-12-05 22:59:21 +0100

    integration

--- github/github-checkruns/github-checkruns.sh
+++ github/github-checkruns/github-checkruns.sh
@@ -63,30 +63,28 @@ while IFS= read -r kustomization; do
     echo $filename
     echo $query
 
-    IFS="$separator" read -r -a kustomization_changed <<< $($here/../generic/filter-lists/filter-lists.sh "$git_changes" "$filename")
+    IFS="$separator" read -r -a kustomization_changed <<< $($here/../../generic/filter-lists/filter-lists.sh "$git_changes" "$filename")
     echo "kustomization_changed: ${#kustomization_changed[@]}"
 
     relative_folder=$(dirname $(realpath $filename --relative-to $KUSTOMIZATION_ROOT))
 
-    IFS="$separator" read -r -a kustomization_resources <<< $($here/../flux/get-all-kustomization-resources/get-all-kustomization-resources.sh $kustomization "$KUSTOMIZATION_ROOT")
+    IFS="$separator" read -r -a kustomization_resources <<< $($here/../../flux/get-all-kustomization-resources/get-all-kustomization-resources.sh $kustomization "$KUSTOMIZATION_ROOT")
     echo "all kustomization_resources: ${kustomization_resources[@]}"
 
-    # echo "(dirname $here/kustomizations/$kustomization_yaml): $(dirname $kustomization_yaml)"
     echo "looking for policy_folders in $relative_folder"
-    IFS="$separator" read -r -a policy_folders <<< $($here/../generic/find-in-ancestor-folders/find-in-ancestor-folders.sh $KUSTOMIZATION_ROOT $relative_folder "policy")
+    IFS="$separator" read -r -a policy_folders <<< $($here/../../generic/find-in-ancestor-folders/find-in-ancestor-folders.sh $KUSTOMIZATION_ROOT $relative_folder "policy")
     echo "policy_folders: ${policy_folders[@]}"
-    # result=$($here/../find-files.sh $here/testdata ".*.ya?ml$" "true")
 
     if [[ "${kustomization_changed}" ]]; then
         resources_to_check="$kustomization_resources"
         resources_to_policy_check="$kustomization_resources"
     else
         # look for resource changes
-        read -r -a filtered_resources <<< $($here/../generic/filter-lists/filter-lists.sh "$git_changes" "${kustomization_resources[*]}")
+        read -r -a filtered_resources <<< $($here/../../generic/filter-lists/filter-lists.sh "$git_changes" "${kustomization_resources[*]}")
         echo "filtered_resources: ${filtered_resources[@]}"
         resources_to_check="$filtered_resources"
 
-        IFS="$separator" read -r -a changed_policy_folders <<< $($here/../generic/filter-changed-directories/filter-changed-directories.sh "$policy_folders" "*" "$git_changes")
+        IFS="$separator" read -r -a changed_policy_folders <<< $($here/../../generic/filter-changed-directories/filter-changed-directories.sh "$policy_folders" "*" "$git_changes")
         echo "policy_folders_changed: ${#changed_policy_folders[@]}"
         if [[ ${#changed_policy_folders[@]} -gt 0 ]]; then
             # policies changed - policy-check all resources
@@ -102,7 +100,7 @@ while IFS= read -r kustomization; do
         echo "resource: $resource"
         if [[ -n "$resource" ]]; then
             echo "calling kubeconform for resource $resource"
-            result=$($here/../kubeval/kubeconform/kubeconform.sh "$resource")
+            result=$($here/../../kubeval/kubeconform/kubeconform.sh "$resource")
             echo $result
         fi
     done < <(tr "$separator" '\n' <<< "${resources_to_check[@]}")
@@ -112,8 +110,7 @@ while IFS= read -r kustomization; do
         echo "resource: $resource"
         if [[ -n "$resource" ]]; then
             echo "calling conftest for resource $resource"
-            # echo "prefixed policy folders: ${policy_folders[@]/#/$here/}"
-            result=$($here/../conftest/conftest-test/conftest.sh "$resource" "${policy_folders[@]}")
+            result=$($here/../../conftest/conftest-test/conftest.sh "$resource" "${policy_folders[@]}")
             echo $result
         fi
     done < <(tr "$separator" '\n' <<< "${resources_to_policy_check[@]}")
