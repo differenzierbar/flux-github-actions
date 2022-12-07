3ced2f03fdfd599558afad3ad368838a4ca6ec65
Tobias Jenkner <tobias.jenkner@gmail.com>
2022-12-07 15:36:07 +0100

    error handling / debugging

--- git/git-changes/git-changes.sh
+++ git/git-changes/git-changes.sh
@@ -9,6 +9,9 @@ directory="${2:-.}"
 
 separator="${SEPARATOR:-$DEFAULT_SEPARATOR}"
 
+>&2 echo "ref: $ref"
+>&2 echo "directory: $directory"
+
 git_changes=$(git -C $directory diff $(git -C $directory merge-base HEAD ${ref}) --name-only --relative)
 
 echo "${git_changes}" | tr "$separator" '\n' | sort -u | tr '\n' "$separator"
