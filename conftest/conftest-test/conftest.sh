f5fa5ab2f0003215805e353de24fbee30d07c028
Tobias Jenkner <tobias.jenkner@gmail.com>
2022-12-07 11:28:17 +0100

    enable gh formatting again

--- conftest/conftest-test/conftest.sh
+++ conftest/conftest-test/conftest.sh
@@ -16,7 +16,7 @@ IFS=$separator read -r -a policy_folders <<< "$(echo $2)"
 # execute conftest
 # if [[ ${#policy_folders[@]} > 0 ]];then
 >&2 echo "executing 'conftest test $input_file ${policy_folders[@]/#/"-p "} -o github'"
-conftest test $input_file ${policy_folders[@]/#/"-p "} # -o github
+conftest test $input_file ${policy_folders[@]/#/"-p "} -o github
 # else
 #     >&2 echo "no policy folders found - skipping conftest for $input_file"
 # fi
