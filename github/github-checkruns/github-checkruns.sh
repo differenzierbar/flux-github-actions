0d9788c72f7a800877c209dc73afcfa433999fd1
Tobias Jenkner <tobias.jenkner@gmail.com>
2022-12-07 12:10:56 +0100

    error propagation

--- github/github-checkruns/github-checkruns.sh
+++ github/github-checkruns/github-checkruns.sh
@@ -1,46 +1,11 @@
 #!/bin/bash
 
+set -e
+
 GITHUB_TOKEN=$1
 GIT_SHA=$2
 KUSTOMIZATION_ROOT=$3
 
-# >&2 echo "GITHUB_TOKEN: $GITHUB_TOKEN"
-# >&2 echo "GIT_SHA: $GIT_SHA"
-# >&2 echo "3: $3"
-
-
-# while IFS= read -r kustomization; do
-#     checkrun=$(curl \
-#     -X POST \
-#     -H "Accept: application/vnd.github+json" \
-#     -H "Authorization: token ${GITHUB_TOKEN}" \
-#     https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs \
-#     --fail \
-#     -d "{\"name\":\"$kustomization\",\"head_sha\":\"${GIT_SHA}\",\"status\":\"in_progress\",\"started_at\":\"$(date -u +"%Y-%m-%dT%H:%M:%S"Z)\",\"output\":{\"title\":\"Mighty Readme report\",\"summary\":\"\",\"text\":\"\"}}")
-#     checkrun_id=$(echo $checkrun | jq .id)
-#     { kustomize_err="$( { kustomize build $3/$kustomization; } 2>&1 1> $3/$kustomization/kustomize.out)"; } || true
-#     if [ -z "$kustomize_err" ]
-#     then
-#     kustomize_out=$(cat $3/$kustomization/kustomize.out | jq -Rsa .)
-#     echo $kustomize_out
-#     checkrun_update="{\"name\":\"$kustomization\",\"status\":\"completed\",\"conclusion\":\"success\",\"completed_at\":\"$(date -u +"%Y-%m-%dT%H:%M:%S"Z)\",\"output\":{\"title\":\"Kustomization output\",\"summary\":\"kustomize build ${kustomzation}\",\"text\":$kustomize_out}}"
-#     else
-#     kustomize_err_esc=$(echo $kustomize_err | jq -Rsa .)
-#     echo $kustomize_err_esc
-#     checkrun_update="{\"name\":\"$kustomization\",\"status\":\"completed\",\"conclusion\":\"failure\",\"completed_at\":\"$(date -u +"%Y-%m-%dT%H:%M:%S"Z)\",\"output\":{\"title\":\"Kustomization error\",\"summary\":\"kustomize build ${kustomzation} failed\",\"text\":$kustomize_err_esc}}"
-#     fi
-#     echo $checkrun_update
-#     curl \
-#     -X PATCH \
-#     -H "Accept: application/vnd.github+json" \
-#     -H "Authorization: token ${GITHUB_TOKEN}" \
-#     https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs/$checkrun_id \
-#     --fail \
-#     -d "$checkrun_update"
-# done <<< "$4"
-
-#!/bin/bash
-set -e
 
 here=`dirname $(realpath $0 --relative-to .)`
 
@@ -51,6 +16,8 @@ kustomizations=$($here/../../flux/find-kustomizations/find-kustomizations.sh $KU
 
 git_changes=$($here/../../git/git-changes/git-changes.sh $GIT_SHA $KUSTOMIZATION_ROOT)
 echo "git_changes: $git_changes"
+
+return_value=0
 
 while IFS= read -r kustomization; do
     # echo "$kustomization"
@@ -99,7 +66,10 @@ while IFS= read -r kustomization; do
             # echo "calling kubeconform for resource $resource"
             # result=$($here/../../kubeval/kubeconform/kubeconform.sh "$resource")
             # echo $result
+            set +e
             $here/../create-checkrun/create-checkrun.sh $GITHUB_TOKEN $GITHUB_HEAD_REF "kubeconform violation in '$resource'" $here/../../kubeval/kubeconform/kubeconform.sh "$resource"
+            return_value|=$?
+            set -e
         fi
     done < <(tr "$separator" '\n' <<< "${resources_to_check[@]}")
 
@@ -110,8 +80,13 @@ while IFS= read -r kustomization; do
             # echo "calling conftest for resource $resource"
             # result=$($here/../../conftest/conftest-test/conftest.sh "$resource" "${policy_folders[@]/#/$KUSTOMIZATION_ROOT/}")
             # echo $result
+            set +e
             $here/../create-checkrun/create-checkrun.sh $GITHUB_TOKEN $GITHUB_HEAD_REF "policy violation in '$resource'" $here/../../conftest/conftest-test/conftest.sh "$resource" "${policy_folders[@]/#/$KUSTOMIZATION_ROOT/}"
+            return_value|=$?
+            set -e
         fi
     done < <(tr "$separator" '\n' <<< "${resources_to_policy_check[@]}")
         
 done < <(tr ' ' '\n' <<< "${kustomizations[@]}")
+
+return return_value
