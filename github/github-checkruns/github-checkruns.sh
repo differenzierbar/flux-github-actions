673e085740f573613f7bb1e7563ebd3e27e4cca1
Tobias Jenkner <tobias.jenkner@gmail.com>
2022-12-08 18:28:53 +0100

    fewer check-runs

--- github/github-checkruns/github-checkruns.sh
+++ github/github-checkruns/github-checkruns.sh
@@ -41,20 +41,31 @@ while IFS= read -r kustomization; do
         resources_to_check=${kustomization_resources[@]}
         resources_to_policy_check=${kustomization_resources[@]}
     else
+        resources_to_check=()
+        resources_to_policy_check=()
         # look for resource changes
-        read -r -a filtered_resources <<< $($here/../../generic/filter-lists/filter-lists.sh "$git_changes" "${kustomization_resources[*]}")
-        echo "filtered_resources: ${filtered_resources[@]}"
-        resources_to_check=${filtered_resources[@]}
+        while IFS= read -r resource; do
+            read -r -a filtered_resources <<< $($here/../../generic/filter-lists/filter-lists.sh "$git_changes" "$resource")
+            echo "filtered_resources: ${filtered_resources[@]}"
 
-        IFS="$separator" read -r -a changed_policy_folders <<< $($here/../../generic/filter-changed-directories/filter-changed-directories.sh "$policy_folders" "*" "$git_changes")
-        echo "policy_folders_changed: ${#changed_policy_folders[@]}"
-        if [[ ${#changed_policy_folders[@]} -gt 0 ]]; then
-            # policies changed - policy-check all resources
-            resources_to_policy_check=${kustomization_resources[@]}
-        else
-            # policies not changed - policy-check only changed resources 
-            resources_to_policy_check=${filtered_resources[@]}
-        fi
+            if [[ ${#filtered_resources[@]} -gt 0 ]];then
+                resources_to_check+=$resource
+                resources_to_policy_check+=$resource
+            else 
+                resource_directory=$(dirname $resource)
+                echo "looking for policy_folders in $resource_directory"
+                IFS="$separator" read -r -a policy_folders <<< $($here/../../generic/find-in-ancestor-folders/find-in-ancestor-folders.sh $KUSTOMIZATION_ROOT $resource_directory "policy")
+                echo "policy_folders: ${policy_folders[@]}"
+
+                IFS="$separator" read -r -a changed_policy_folders <<< $($here/../../generic/filter-changed-directories/filter-changed-directories.sh "$policy_folders" "*" "$git_changes")
+                echo "policy_folders_changed: ${#changed_policy_folders[@]}"
+                if [[ ${#changed_policy_folders[@]} -gt 0 ]]; then
+                    # policies changed - policy-check all resources
+                    resources_to_policy_check+=$resource
+                fi
+            fi
+        done < <(tr "$separator" '\n' <<< "${resources_to_check[@]}")
+
     fi
 
 
@@ -109,7 +120,7 @@ while IFS= read -r kustomization; do
         summary="failed checks"
     fi
     
-    $here/../create-checkrun/create-checkrun.sh $GITHUB_TOKEN $GITHUB_HEAD_REF "conftest test '$kustomization'" $conclusion $summary $text
+    $here/../create-checkrun/create-checkrun.sh $GITHUB_TOKEN $GITHUB_SHA "'$filename'" $conclusion $summary $text
 
         
 done < <(tr ' ' '\n' <<< "${kustomizations[@]}")
