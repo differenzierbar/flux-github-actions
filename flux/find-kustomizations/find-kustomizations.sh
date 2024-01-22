10d988ee3537e2e65176f1f4160f155496a428f3
Tobias Jenkner <tobias.jenkner@gmail.com>
2024-01-22 17:49:49 +0100

    search for flux kustomizations with wildcard version

--- flux/find-kustomizations/find-kustomizations.sh
+++ flux/find-kustomizations/find-kustomizations.sh
@@ -11,7 +11,7 @@ query=$2
 >&2 echo "query: $query"
 >&2 echo "separator: '$separator'"
 
-result=$(find $kustomizations_root -name "*.yml" -exec yq -N eval '. | select(.kind == "Kustomization" and .apiVersion == "kustomize.toolkit.fluxcd.io/v1beta2" and ('$query') ) | filename + "?metadata.name==\"" + .metadata.name + "\""' {} +)
+result=$(find $kustomizations_root -name "*.yml" -exec yq -N eval '. | select(.kind == "Kustomization" and .apiVersion == "kustomize.toolkit.fluxcd.io/*" and ('$query') ) | filename + "?metadata.name==\"" + .metadata.name + "\""' {} +)
 # >&2 echo "result inner: ${result[@]}"
 echo "${result[@]}" | tr "$separator" '\n' | sort -u | tr '\n' "$separator" | sed 's/ *$//g'
 
